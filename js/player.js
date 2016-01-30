07d7edda8dcd1c06684771368b300f4c66634350
Warren Wegenke <wjwegg@gmail.com>
2016-01-30 02:17:09 -0600

    Update player

--- js/player.js
+++ js/player.js
@@ -8,6 +8,9 @@ function char(){
 	this.getScore = function(){
 		return this.score;
 	}
+	this.getPoints = function() {
+		return this.points;
+	}
 	this.getName = function(){
 		return this.name;
 	}
@@ -57,6 +60,10 @@ function char(){
 	this.addItem = function() {
 		weapons.push("item");
 	}
+	
+	this.randCall = function() {
+		Math.floor((Math.random() * 10) + 1); //Random from 1 to 10
+	}
 }
 
 function monster(name,hp,lvl,drop){
@@ -72,7 +79,7 @@ function monster(name,hp,lvl,drop){
         return this.hp;
     }  
 	this.getLvl = function() {
-		return this.getLvl;
+		return this.lvl;
 	}
 	this.getDrop = function() {
 		return this.drop;
