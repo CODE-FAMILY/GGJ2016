2dc3851c1d358a342b105acf5ac8be636f6735d0
totalwar235 <rbnorstrom@pluto.dsu.edu>
2016-01-30 14:13:43 -0600

    add a header js file

---
+++ js/Jsfileheaders.js
@@ -0,0 +1,10 @@
+function header(){
+    document.write("<script src=\"js/css-popup.js\"></script>");
+    document.write("<script src=\"js/item.js.js\"></script>");
+    document.write("<script src=\"js/player.js\"></script>");
+    document.write("<script src=\"js/enemyObj.js\"></script>");
+    document.write("<script src=\"js/highscores.js\"></script>");
+    document.write("<script src=\"js/itemObj.js\"></script>");
+    document.write("<script src=\"js/menu.js\"></script>");
+    document.write("<script src=\"js/summon.js\"></script>");
+}
