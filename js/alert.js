b32848ff9825f49a422855a11756769219426a40
robsalas <rsalas@pluto.dsu.edu>
2016-01-30 13:06:22 -0600

    Remove

--- js/alert.js
+++
@@ -1,9 +0,0 @@
-function AlertFunction() 
-{
-    alert("Hello! I am an alert box! Hello! I am an alert box! Hello! I am an alert box! Hello! I am an alert box! ");
-}
-
-function redirect()
-{
-        window.location='Page2.html';
-}
