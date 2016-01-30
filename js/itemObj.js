585cd8c2e48aa6a5df2b0d30c76995816bb00157
Warren Wegenke <wjwegg@gmail.com>
2016-01-30 13:20:06 -0600

    Added enemies. Updated item and player

--- js/itemObj.js
+++ js/itemObj.js
@@ -1,62 +1,55 @@
 var playa = new char();
-//playa.hp = 100;
 
-/*var fist = item();
+var fist = new item();
 fist.name = "Fist";
 fist.power = 1;
 fist.speed = 1;
 fist.cond = 5;
 
-var dagger = item();
+var dagger = new item();
 dagger.name = "Dagger";
 dagger.power = 2;
 dagger.speed = 2;
 dagger.cond = 5;
 
-var sword = item();
+var sword = new item();
 sword.name = "Sword";
 sword.power = 3;
 sword.speed = 1;
 sword.cond = 5;
 
-var whip = item();
+var whip = new item();
 whip.name = "Whip";
 whip.power = 2;
 whip.speed = 3;
 whip.cond = 5;
 
-var wand = item();
+var wand = new item();
 wand.name = "Wand";
 wand.power = 2;
 wand.speed = 4;
 wand.cond = 5;
 
-var pistol = item();
+var pistol = new item();
 pistol.name = "Pistol";
 pistol.power = 3;
 pistol.speed = 3;
 pistol.cond = 5;
 
-var rifle = item();
+var rifle = new item();
 rifle.name = "Rifle";
 rifle.power = 4;
 rifle.speed = 3;
 rifle.cond = 5;
 
-var rocket = item();
+var rocket = new item();
 rocket.name = "Rocket";
 rocket.power = 5;
 rocket.speed = 1;
 rocket.cond = 5;
 
-var rayGun = item();
+var rayGun = new item();
 rayGun.name = "Ray-Gun";
 rayGun.power = 2;
 rayGun.speed = 5;
 rayGun.cond = 5;
-
-var commonEnemy = monster();
-    commonEnemy.name = name;
-    commonEnemy.hp = 100;
-    commonEnemy.lvl = 1;
-    commonEnemy.drop = drop;*/
