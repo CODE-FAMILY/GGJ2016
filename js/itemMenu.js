1a20bd79153066bc88817d1043033982c55c0464
Jeeppler <jeremias.eppler@mailbox.org>
2016-01-31 08:09:31 -0600

    default 100%

--- js/itemMenu.js
+++ js/itemMenu.js
@@ -4,8 +4,13 @@ function itemScreen(){
     var itemImage = document.getElementById("item-image");
     var weaponName = document.getElementById("weapon-name");
 
+	weaponName.childNodes[0].nodeValue = "hello";
+
+    console.log( weaponName.textContent );
+    console.log( "a" );
+
     //itemImage.href.setAttribute("baseVal", playa.items[playa.getEquiped()].filePath);
-    weaponName.text = "hello"; //playa.items[playa.getEquiped()].name;
+    //weaponName.textContent = "hello"; //playa.items[playa.getEquiped()].name;
     //document.getElementById("item").style.display = "block";
 }
 
