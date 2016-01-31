a9492952b38e0b43ca1986559ad98dbd4f4aae9c
totalwar235 <rbnorstrom@pluto.dsu.edu>
2016-01-31 14:23:53 -0600

    hi

--- js/basic.js
+++ js/basic.js
@@ -1,24 +1,21 @@
-function init(){
-    
-    commonEnemy = starter.clone();
-    document.getElementById("monstImage").src = (commonEnemy.enemyImg);
-    document.getElementById("monstHealth").style.width = "100%";
-    
-    var starterQ = new quest();
-    starterQ.id = -1;
-    starterQ.limit = 1;
-    starterQ.monster = starter.clone();
-    starterQ
-    
-    startPopUp();
+var toggleMute = true;
+var toggleAni = true;
+
+function animation(){
+    if(toggleAni){
+        toggleAni = false;
+    }
+    else{
+        toggleAni = true;
+    }
     
 }
 
-function startPopUp(){
-    document.write("<div id=\"blanket\" style=\"display:none\"> </div>");
-    document.write("<div id=\"popUpStart\" style=\"display:none\">");
-    document.write("<h2>The Summoning</h2>");
-    document.write("<p>   You must click, and click the monster on the top right until you kill enough monsters and click enough times to join the holy order of the Clickers.</p>");
-    document.write("<button href=\"#\" onclick=\"popup('popUpStart')\" >I am ready to complete my quest</button>");
-    document.write("</div>");
+function mute(){
+    if(toggleMute){
+        toggleMute = false;
+    }
+    else{
+        toggleMute = true;
+    }
 }
