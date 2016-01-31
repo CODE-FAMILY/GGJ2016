7da53b554924de165445c37257abcc8c2a3112a2
totalwar235 <rbnorstrom@pluto.dsu.edu>
2016-01-30 22:34:26 -0600

    for warren to work on

--- js/basic.js
+++ js/basic.js
@@ -4,13 +4,21 @@ function init(){
     document.getElementById("monstImage").src = (commonEnemy.enemyImg);
     document.getElementById("monstHealth").style.width = "100%";
     
+    var starterQ = new quest();
+    starterQ.id = -1;
+    starterQ.limit = 1;
+    starterQ.monster = starter.clone();
+    starterQ
+    
     startPopUp();
+    
 }
 
 function startPopUp(){
     document.write("<div id=\"blanket\" style=\"display:none\"> </div>");
     document.write("<div id=\"popUpStart\" style=\"display:none\">");
-    document.write("<button href=\"#\" onclick=\"popup('popUpStart')\" >Click to Close CSS Pop Up</button>");
+    document.write("<h2>The Summoning</h2>");
+    document.write("<p>   You must click, and click the monster on the top right until you kill enough monsters and click enough times to join the holy order of the Clickers.</p>");
+    document.write("<button href=\"#\" onclick=\"popup('popUpStart')\" >I am ready to complete my quest</button>");
     document.write("</div>");
-    document.write("<button href=\"#\" onclick=\"popup('popUpStart')\">Click to Open CSS Pop Up</button>");
 }
